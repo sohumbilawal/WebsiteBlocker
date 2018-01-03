a817ac583ad7c933c8e36cd8f18045ffc385b010
sohumbilawal <sohumbilawal@gmail.com>
2018-01-02 17:40:51 -0800

    Only scheduling to go.
    Currently, this is a working copy that will work when you have admin access to a terminal on windows.
    Scheduling coming up.

--- webBlock.py
+++ webBlock.py
@@ -1,14 +1,29 @@
 import time
 from datetime import datetime as dt
 
+hosts = "hosts"
 hosts_path = "C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
-web_list = ["www.facebook.com", "facebook.com", "https://mail.google.com/mail/u/0/#inbox"]
+web_list = ["www.facebook.com", "facebook.com", "mail.google.com", "www.mail.google.com", "mail.google.com/mail/?authuser=0"]
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 16) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16, 55):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 16) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
         print("Working hours...")
-        #print(1)
+        with open(hosts_path, 'r+') as file:
+            hosts_content = file.read()
+            for website in web_list:
+                if website in hosts_content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+
     else:
+        with open(hosts_path, 'r+') as file:
+            hosts_content=file.readlines()
+            file.seek(0)
+            for line in hosts_content:
+                if not any(website in line for website in web_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours...")
     time.sleep(5)
