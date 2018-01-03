328a44eeae57ff73e8cc0156845123dd62fa8ffd
sohumbilawal <sohumbilawal@gmail.com>
2018-01-02 16:57:21 -0800

    While loop structure complete
    Compares datetime objects to each other, and to current time, and prints out the appropriate statement accordingly.

--- webBlock.py
+++ webBlock.py
@@ -1,9 +1,14 @@
 import time
+from datetime import datetime as dt
+
 hosts_path = "C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 web_list = ["www.facebook.com", "facebook.com", "https://mail.google.com/mail/u/0/#inbox"]
 
 while True:
-
-    print(1)
-    time.sleep(300)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 16) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16, 55):
+        print("Working hours...")
+        #print(1)
+    else:
+        print("Fun hours...")
+    time.sleep(5)
