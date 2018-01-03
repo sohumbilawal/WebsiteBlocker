a2d1460b5442642d3d4c07d9b35191132b529de5
sohumbilawal <sohumbilawal@gmail.com>
2018-01-02 16:43:33 -0800

    Adding a while loop to check time
    ... and for Python to be able to run the program constantly in the background.

---
+++ webBlock.py
@@ -0,0 +1,9 @@
+import time
+hosts_path = "C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+web_list = ["www.facebook.com", "facebook.com", "https://mail.google.com/mail/u/0/#inbox"]
+
+while True:
+
+    print(1)
+    time.sleep(300)
